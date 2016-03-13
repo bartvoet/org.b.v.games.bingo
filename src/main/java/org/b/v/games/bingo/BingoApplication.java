44d3446e1cff54608a5af1c286a2bbf3cc2d91fd
bart <bart_voet@telenet.be>
2016-03-13 11:06:31 +0100

    [restructuring] Moving the buttons to the applicatoin away from the BingoPanel to allow later refactoring

--- src/main/java/org/b/v/games/bingo/BingoApplication.java
+++ src/main/java/org/b/v/games/bingo/BingoApplication.java
@@ -1,14 +1,17 @@
 package org.b.v.games.bingo;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 
 public class BingoApplication {
 	
@@ -16,8 +19,18 @@ public class BingoApplication {
 	private static final int DEFAULT_GRID_COLS = 10;
 
 	private static JFrame mainScreen;
+	private static BingoPanel mainPanel;
 	private static JFrame secondScreen;
-	private static BingoPanel bingoPanel;
+	private static BingoPanel secondPanel;
+	
+	private static final String NEXT_LABEL = "volgende";
+	private static final String RESTART_LABEL = "nieuw spel";
+	
+	private static JButton nextButton = new JButton(NEXT_LABEL);
+	private static JButton restartButton = new JButton(RESTART_LABEL);
+	
+	
+	private BingoGame game;//TODO let the app interact with screen and play master (rather that frame)
 	
 	private static BingoPanel contentPane(String[] args) {
 		if(args.length < 2) {
@@ -50,6 +63,22 @@ public class BingoApplication {
         menuBar.add(menu);
 	}
 	
+	private static void configureSomeActions() {
+		nextButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mainPanel.makeAGuess();
+			}
+		});
+		
+		restartButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				mainPanel.clean();
+			}
+		});
+	}
+	
 	public static void main(String[] args) {
 		mainScreen = new JFrame();
 		
@@ -57,9 +86,24 @@ public class BingoApplication {
 		mainScreen.setJMenuBar(menuBar);
 		initializeTheAdminstrationMenu(menuBar);
 		
-		bingoPanel = contentPane(args);
+		JPanel mainScreenPanel = new JPanel();
+		mainScreenPanel.setLayout(new BorderLayout());
+		mainPanel = contentPane(args);
+		mainScreenPanel.add(mainPanel,BorderLayout.CENTER);
 		
-		mainScreen.setContentPane(bingoPanel);
+		
+		nextButton = new JButton(NEXT_LABEL);
+		restartButton = new JButton(RESTART_LABEL);
+		configureSomeActions();
+		
+		JPanel panel = new JPanel();
+		panel.add(nextButton);
+		panel.add(restartButton);
+		mainScreenPanel.add(panel,BorderLayout.PAGE_END);
+		
+		secondPanel = contentPane(args);
+		
+		mainScreen.setContentPane(mainScreenPanel);
 		mainScreen.setExtendedState(mainScreen.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 		mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainScreen.pack();
--- src/main/java/org/b/v/games/bingo/BingoPanel.java
+++ src/main/java/org/b/v/games/bingo/BingoPanel.java
@@ -2,10 +2,7 @@ package org.b.v.games.bingo;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
@@ -14,14 +11,10 @@ public class BingoPanel extends JPanel {
 	private static final long serialVersionUID = 3640685537306132479L;
 
 	private static final String INITIAL_TOP_LABEL = "?";
-	private static final String NEXT_LABEL = "volgende";
-	private static final String RESTART_LABEL = "nieuw spel";
 	
 	private static final int SIZE_OF_TOP_LABEL = 400;
 	private static final int SIZE_OF_GRID_LABEL = 20;
 
-	private JButton nextButton;
-	private JButton restartButton;
 	private JLabel topLabel;
 
 	private GridPanel grid;
@@ -29,39 +22,24 @@ public class BingoPanel extends JPanel {
 	
 	public BingoPanel(int rows,int cols) {
 		initializeTheComponents(rows, cols);
-		configureSomeActions();
 		doScreenSettings();
 		doSomeLayout();
 	}
 
 	private void initializeTheComponents(int rows, int cols) {
-		nextButton = new JButton(NEXT_LABEL);
-		restartButton = new JButton(RESTART_LABEL);
 		topLabel = new JLabel(INITIAL_TOP_LABEL,SwingConstants.CENTER);
 		
 		game = new BingoGame(rows * cols);
 		//grid = GridPanel.fromLeftToRight(rows,cols,SIZE_OF_GRID_LABEL);
 		grid = GridPanel.fromTopToBottom(cols,rows,SIZE_OF_GRID_LABEL);
 	}
-
-	private void configureSomeActions() {
-		nextButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				makeAGuess();
-			}
-		});
-		
-		restartButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				grid.clean();
-				game.clean();
-				topLabel.setText(INITIAL_TOP_LABEL);
-			}
-		});
-	}
 	
+	public void clean() {
+		grid.clean();
+		game.clean();
+		topLabel.setText(INITIAL_TOP_LABEL);
+	}
+
 	private void doScreenSettings() {
 		setLayout(new BorderLayout());
 	}
@@ -71,18 +49,13 @@ public class BingoPanel extends JPanel {
 		
 		this.add(topLabel,BorderLayout.PAGE_START);
 		this.add(grid,BorderLayout.CENTER);
-		
-		JPanel panel = new JPanel();
-		panel.add(nextButton);
-		panel.add(restartButton);
-		this.add(panel,BorderLayout.PAGE_END);
 	}
 
 	private void setLabelSize(JLabel label,int size) {
 		label.setFont(new Font(label.getFont().getName(), label.getFont().getStyle(), size));
 	}
 
-	private void makeAGuess() {
+	public void makeAGuess() {
 		if(!game.isEverythingsAlreadySelected()) {
 			markTheSelectedNumberOnTheScreen(game.guessSelection());
 		}
